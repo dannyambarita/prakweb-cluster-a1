ea6c2a3d2ec82c29c9241cae4dc37392ac7fa4df
dannyambarita <danny.ambarita130@gmail.com>
2020-11-22 14:37:32 +0700

    nomor 2 new

--- Modul 6/nomor 2.php
+++ Modul 6/nomor 2.php
@@ -5,14 +5,17 @@ function bet($name, $color = "red")
     $jumlah = strlen($name);
     if ($jumlah <= 10 && $jumlah >= 1) {
         $harga = $jumlah * 300;
-        echo " $harga";
+        echo '<font color="' . $color . '"> ' . $harga .  '</font>';
     } elseif ($jumlah <= 20 && $jumlah >= 11) {
         $harga = $jumlah * 500;
-        echo " $harga";
+        echo '<font color="' . $color . '"> ' . $harga .  '</font>';
     } else {
         $harga = $jumlah * 700;
-        echo " $harga";
+        echo '<font color="' . $color . '"> ' . $harga .  '</font>';
     }
+    echo "</br>";
 }
 
 $hasil = bet('Christio Danny Gratia Ambarita', 'black');
+$hasil = bet('Danny Ambarita', 'grey');
+$hasil = bet('Christio');
